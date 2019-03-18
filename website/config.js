a17edc87ea7d4b044f2c39b69882277db69709a0
Kevin Guay <kguay@bigelow.org>
2019-03-18 14:29:45 -0400

    Initial commit

---
+++ website/config.js
@@ -0,0 +1,8 @@
+var meetingName = { 2073152566: "Boadroom",
+                    2073152567: "Classroom",
+                    2073152568: "Alfond Room",
+                    2073152000: "Webinar",
+                    2073152561: "Pro One",
+                    2073152562: "Pro Two",
+                    2073152563: "Pro Three",
+                    2073152564: "Pro Four"};
